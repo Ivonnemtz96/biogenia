0edcf8d7b54758ab4f3651d9f0addbc6c54a1231
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-18 09:11:24 -0700

    Se agrega diseño al php

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+    
+    <!-- link of css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/lightcase.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Medicap</title>
+</head>
---
+++ includes/header.php
@@ -0,0 +1,120 @@
+<!-- desktop menu start here -->
+<header class="header-section bg-ash">
+    <div class="header-area">
+        <div class="container">
+            <div class="primary-menu">
+                <div class="logo py-2">
+                    <a href="index.html"><img src="assets/images/logo/02.png" alt="logo"></a>
+                </div>
+                <div class="menu-bar d-lg-none">
+                    <i class="icofont-navigation-menu"></i>
+                </div>
+                <div class="main-area">
+                    <div class="main-menu d-flex flex-wrap align-items-center justify-content-center w-100">
+                        <ul class="lab-ul">
+                            <li class="active">
+                                <a href="#0">Home</a>
+                                <ul class="lab-ul">
+                                    <li class="active"><a href="index.html">Home Page One</a></li>
+                                    <li><a href="index-2.html">Home Page Two</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="about.html">About</a></li>
+                            <li>
+                                <a href="#0">Pages</a>
+                                <ul class="lab-ul">
+                                    <li>
+                                        <a href="#0">Services</a>
+                                        <ul class="lab-ul">
+                                            <li><a href="service.html">Service</a></li>
+                                            <li><a href="service-single.html">Service Single</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#0">Portfolios</a>
+                                        <ul class="lab-ul">
+                                            <li><a href="portfolio.html">Portfolio style 1</a></li>
+                                            <li><a href="portfolio-2.html">Portfolio style 2</a></li>
+                                            <li><a href="portfolio-single.html">Portfolio style 3</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="team.html">Team Membar</a></li>
+                                    <li><a href="timetable.html">Time Table</a></li>
+                                    <li><a href="history.html">History Page</a></li>
+                                    <li><a href="404.html">404 Page</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#0">Blog</a>
+                                <ul class="lab-ul">
+                                    <li><a href="blog.html">Blog Style 1</a></li>
+                                    <li><a href="blog-2.html">Blog style 2</a></li>
+                                    <li><a href="blog-3.html">Blog style 3</a></li>
+                                    <li><a href="blog-single.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#0">Shop</a>
+                                <ul class="lab-ul">
+                                    <li><a href="shop.html">Products Page</a></li>
+                                    <li><a href="shop-single.html">Products Details</a></li>
+                                    <li><a href="cart-page.html">Cart Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                        <ul class="lab-ul search-cart">
+                            <li>
+                                <div class="search-option">
+                                    <div class="icon icon-search"><i class="icofont-search-2"></i></div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cart-option">
+                                    <div class="icon icon-check"><i class="icofont-cart-alt"></i></div>
+                                    <span class="count-item">2</span>
+                                    <div class="cart-content">
+                                        <div class="cart-item">
+                                            <div class="cart-img">
+                                                <img src="assets/images/shop/01.jpg" alt="cart">
+                                            </div>
+                                            <div class="cart-des">
+                                                <a href="#">Product Title Hore</a>
+                                                <p>$20.00</p>
+                                            </div>
+                                            <div class="cart-btn">
+                                                <a href="#"><i class="icofont-close-circled"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="cart-item">
+                                            <div class="cart-img">
+                                                <img src="assets/images/shop/02.jpg" alt="cart">
+                                            </div>
+                                            <div class="cart-des">
+                                                <a href="#">Product Title Hore</a>
+                                                <p>$20.00</p>
+                                            </div>
+                                            <div class="cart-btn">
+                                                <a href="#"><i class="icofont-close-circled"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="cart-bottom">
+                                            <div class="cart-subtotal">
+                                                <p>Total: <b class="float-right">$40.00</b></p>
+                                            </div>
+                                            <div class="cart-action">
+                                                <a href="#" class="lab-btn"><span>View Cart</span></a>
+                                                <a href="#" class="lab-btn"><span>Check Out</span></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- desktop menu ending here -->

3ec215f33ee61299261365a530f4200f4bb844c2
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-18 16:21:39 -0700

    Se agrega Nosotros y diseño

--- modules/nosotros.php
+++ modules/nosotros.php
@@ -4,11 +4,11 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-header-area">
-                    <h3 class="page-header-title">About Our Clinic</h3>
+                    <h3 class="page-header-title">Sobre Nosotros</h3>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb bg-transparent mb-0">
-                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">About</li>
+                            <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Nosotros</li>
                         </ol>
                     </nav>
                 </div>
@@ -31,8 +31,8 @@
                                 <img src="assets/images/care/icon/01.png" alt="care">
                             </div>
                             <div class="pctc-content">
-                                <h3>20+ <span>Years of </span></h3>
-                                <p>Experience medicap Hospital</p>
+                                <h3><span>1er Laboratorio </span></h3>
+                                <p>De fecundación in Vitro en BCS.</p>
                             </div>
                         </div>
                     </div>
@@ -41,8 +41,7 @@
             <div class="col-lg-6 col-12">
                 <div class="pc-content">
                     <div class="pc-title">
-                        <h2><span class="d-block theme-color">Welcome To medicap</span> We’re Setting The Standards in
-                            Hospital & Clinical Care</h2>
+                        <h2><span class="d-block theme-color">Biogenia</span> El primer laboratorio de <b>fecundación in vitro</b> en BCS</h2>
                     </div>
                     <div class="pc-details">
                         <p>Enthusiastica envisioneer interactive process improvements without inexpensve strategic theme
@@ -67,3 +66,128 @@
     </div>
 </div>
 <!-- pasent care section ending here -->
+
+<!-- Start counter Section -->
+<section class="counter-section style-1">
+    <div class="container">
+        <div class="section-wrapper">
+            <div class="counter-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s">
+                <div class="counter-item-inner">
+                    <div class="counter-thumb">
+                        <img src="assets/images/counter/01.png" alt="counter">
+                    </div>
+                    <div class="counter-content">
+                        <h3 class="count-number"><span class="counter">731</span></h3>
+                        <p class="post-content">Our Volunteer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="counter-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
+                <div class="counter-item-inner">
+                    <div class="counter-thumb">
+                        <img src="assets/images/counter/02.png" alt="counter">
+                    </div>
+                    <div class="counter-content">
+                        <h3 class="count-number"><span class="counter">30</span></h3>
+                        <p class="post-content">Happy Children</p>
+                    </div>
+                </div>
+            </div>
+            <div class="counter-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                <div class="counter-item-inner">
+                    <div class="counter-thumb">
+                        <img src="assets/images/counter/03.png" alt="counter">
+                    </div>
+                    <div class="counter-content">
+                        <h3 class="count-number"><span class="counter">30</span><span>K</span></h3>
+                        <p class="post-content">Total Donated</p>
+                    </div>
+                </div>
+            </div>
+            <div class="counter-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
+                <div class="counter-item-inner">
+                    <div class="counter-thumb">
+                        <img src="assets/images/counter/04.png" alt="counter">
+                    </div>
+                    <div class="counter-content">
+                        <h3 class="count-number"><span class="counter">30</span></h3>
+                        <p class="post-content">Products & Gifts</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End counter Section -->
+
+<!-- Team Sections Start Here -->
+<section class="team-section relative padding-tb">
+    <div class="pattan-shape"></div>
+    <div class="container">
+        <div class="section-header">
+            <h2><span class="d-block">Who We Are</span> Meet Our <span class="theme-color">Professional Doctors</span>
+            </h2>
+        </div>
+        <div class="section-wrapper">
+            <div class="row justify-content-center no-gutters">
+                <div class="col-lg-6 col-12">
+                    <div class="team-item style-1">
+                        <div class="team-inner">
+                            <div class="team-thumb">
+                                <img src="assets/images/team/01.jpg" alt="team-thumb">
+                            </div>
+                            <div class="team-content">
+                                <h5 class="member-name">Dr. Jason Kovalsky</h5>
+                                <span class="member-dagi">Sergion Specialist</span>
+                                <p class="member-details">Dr. Jason Kovalsky is and internist in rochester and has been
+                                    in practilce between 5-8 years </p>
+                                <ul class="lab-ul">
+                                    <li><span>Speciality</span>: Sergion Specialist</li>
+                                    <li><span>Degrees</span>: M.D. of Medicine</li>
+                                    <li><span>Experiences</span>: 06+ Years Ago</li>
+                                    <li><span>Follow us</span>: <a href="#"><i class="icofont-facebook"></i></a><a
+                                            href="#"><i class="icofont-twitter"></i></a><a href="#"><i
+                                                class="icofont-skype"></i></a><a href="#"><i
+                                                class="icofont-instagram"></i></a><a href="#"><i
+                                                class="icofont-pinterest"></i></a></li>
+                                </ul>
+                                <a href="team-single.html" class="text-btn">View Portfolio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="team-item style-1">
+                        <div class="team-inner">
+                            <div class="team-thumb">
+                                <img src="assets/images/team/02.jpg" alt="team-thumb">
+                            </div>
+                            <div class="team-content">
+                                <h5 class="member-name">Dr. Bangile Yusha</h5>
+                                <span class="member-dagi">Sergion Specialist</span>
+                                <p class="member-details">Dr. Bangile Yusha is and internist in rochester and has been
+                                    in practilce between 5-8 years </p>
+                                <ul class="lab-ul">
+                                    <li><span>Speciality</span>: Sergion Specialist</li>
+                                    <li><span>Degrees</span>: M.D. of Medicine</li>
+                                    <li><span>Experiences</span>: 06+ Years Ago</li>
+                                    <li><span>Follow us</span>: <a href="#"><i class="icofont-facebook"></i></a><a
+                                            href="#"><i class="icofont-twitter"></i></a><a href="#"><i
+                                                class="icofont-skype"></i></a><a href="#"><i
+                                                class="icofont-instagram"></i></a><a href="#"><i
+                                                class="icofont-pinterest"></i></a></li>
+                                </ul>
+                                <a href="team-single.html" class="text-btn">View Portfolio</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <a href="team.html" class="lab-btn"><span>View All Doctors<i
+                            class="icofont-double-right"></i></span></a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Team Sections Ending Here -->
